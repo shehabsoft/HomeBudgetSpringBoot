4c7a0b1e9b9b7f5896c2101dcdd7c074683dbeed
Shehab Tarek <shehab.tarek@intellisc.com>
2020-02-29 17:08:43 +0200

    CROS POLICY
    CROS POLICY

---
+++ src/main/java/com/homeBudget/SpringConfig.java
@@ -0,0 +1,20 @@
+package com.homeBudget;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by shehab.tarek on 2/29/2020.
+ */
+@Configuration
+@EnableWebMvc
+public class SpringConfig
+    implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("https://aqueous-temple-30838.herokuapp.com/");
+        }
+}
